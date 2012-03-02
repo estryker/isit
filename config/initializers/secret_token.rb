02214018abb16ad91663c381869d4547a0a650e6
Ethan Stryker <e.stryker@gmail.com>
2012-03-01 21:51:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Istherea::Application.config.secret_token = '1364c88c6260403f7488585760ed3997011b319a296520c23a5f31afb3273ae26ba90e90c4a299e13a8e6a3c10590d15e5925158315838a2da7ec877b518cec9'
