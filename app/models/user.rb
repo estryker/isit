8659d02e7f9e9abd5e558f2e1225d63787171c42
Ethan Stryker <e.stryker@gmail.com>
2013-01-22 11:54:06 -0500

    added first_name last_name to attr_accessor in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,5 +32,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # The devise controller uses these to set attributes, so add them here as the model grows
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
 end
