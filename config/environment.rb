e9abfd41d062fa989c2bff41e2d2abb7037ea371
Ethan Stryker <e.stryker@gmail.com>
2013-01-23 12:32:43 -0500

    added email confirmations to registration and changes to email address

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.mandrillapp.com",
+  :port  => 25,
+  :user_name  => "app11204151@heroku.com",
+  :password  => "nqUDN-wSNJ55eMtPL7CC5Q",
+  :authentication  => :login
+}
 # Initialize the rails application
 Istherea::Application.initialize!
