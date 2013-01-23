1e573216a60ed4a7d1493a4725d941afc2f10827
Ethan Stryker <e.stryker@gmail.com>
2013-01-23 12:35:46 -0500

    updated smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,14 @@ Istherea::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address  => "smtp.mandrillapp.com",
+    :port  => 25,
+    :user_name  => "app11204151@heroku.com",
+    :password  => "nqUDN-wSNJ55eMtPL7CC5Q",
+    :authentication  => :login
+  }
 end
