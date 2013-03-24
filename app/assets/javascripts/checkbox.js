66e84d9a4dff30add48c4d7eb5cd07b3f62076b8
Ethan Stryker <e.stryker@gmail.com>
2013-03-23 21:58:54 -0400

    updating check boxes

--- app/assets/javascripts/checkbox.js
+++ app/assets/javascripts/checkbox.js
@@ -1,7 +1,7 @@
 $(function(){
     $('#authorize').simpleImageCheck({
-	image: '/assets/unchecked.png',             // String The image source to show when the checkbox IS NOT checked (REQUIRED) 
-	imageChecked: '/assets/checked.png',        // String The image source to show when the checkbox IS checked (REQUIRED)
+	image: '/assets/uncheck_box_66.png',             // String The image source to show when the checkbox IS NOT checked (REQUIRED) 
+	imageChecked: '/assets/check_box_66.png',        // String The image source to show when the checkbox IS checked (REQUIRED)
 	afterCheck: function(isChecked) {   // Function Optional callback function for when the image/checkbox is toggled
             // do something if isChecked === true
 	}
